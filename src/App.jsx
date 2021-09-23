c003d016c12091a6ccd0b95b064f331c3038cf54
Danijel Buhin <danijel@danijelbuhin.com>
2021-09-23 09:32:09 +0200

    Create popup and button

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import React, { useState, useEffect } from 'react';
+import { Popup } from './components/Popup/Popup';
 
 function App() {
+  const [isVisible, setIsVisible] = useState(false);
+
   return (
     <div className="App">
+      <button onClick={() => setIsVisible(true)}>Trigger</button>
+
+      {isVisible && (
+        <Popup
+          title="Hey you!!!!!!"
+          confirmText="Yes"
+          cancelText="No, I don't"
+          description="Do you want to proceed with the game?"
+          onConfirm={() => setIsVisible(false)}
+          onCancel={() => setIsVisible(false)}
+        />
+      )}
     </div>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import './index.css';
 
-window.renderApp = () => {
-
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById('root'),
-  );
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
 // Learn more: https://www.snowpack.dev/concepts/hot-module-replacement
