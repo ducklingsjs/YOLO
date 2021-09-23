c013e3dd53e03ba5689c2ed0f64cd906fba3f544
doroz0 <dario.oroz@infinum.hr>
2021-09-23 10:40:12 +0200

    gamo

--- server.js
+++ server.js
@@ -1,15 +1,47 @@
 const app = require('express')();
-const http = require('http').Server(app);
+const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const cors = require('cors');
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+app.use(cors());
+
+const players = {};
+const orderOfPlayers = [];
+let currentPlayer = 0;
 
 io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
+  socket.on('msg', (msg) => {
+    const [username, position] = msg.split(':');
+    socket.username = username;
+
+    if (!orderOfPlayers.length) {
+      io.emit('msg', `host:${username}`);
+    }
+
+    if (!orderOfPlayers.includes(username)) {
+      console.log(`New player - ${username}${!orderOfPlayers.length ? ' - also host' : ''}`);
+      orderOfPlayers.push(username);
+    } else if (msg.startsWith('p:')) {
+      currentPlayer = (currentPlayer + 1) % orderOfPlayers.length;
+      io.emit('msg', `nextplayer:${orderOfPlayers[currentPlayer]}`);
+      console.log(`Next player - ${orderOfPlayers[currentPlayer]}`);
+    }
+    players[username] = parseInt(position);
+
+    io.emit('msg', msg);
+  });
+
+  socket.on('disconnect', () => {
+    if (!players[socket.username] || !socket.username) return;
+    console.log('Disconnecting ' + socket.username);
+    delete players[socket.username];
+    const i = orderOfPlayers.indexOf(socket.username);
+    if (i >= 0) {
+      orderOfPlayers.splice(i, 1);
+      currentPlayer = currentPlayer % orderOfPlayers.length;
+      io.emit('msg', `nextplayer:${orderOfPlayers[currentPlayer]}`);
+    }
   });
 });
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
+import './socket.io.min.js';
 
-import f13_img from './levels/f13.jpg';
-import f13 from './levels/f13.json';
+import level_img from './levels/floor-13.png';
+import level from './levels/f13.json';
+
+const getPointColorForType = ({ type }) => {
+  switch (type) {
+    case 'start':
+      return 'darkgoldenrod';
+    case 'wc':
+      return 'slategray';
+    case 'call':
+      return 'mediumturquoise';
+    case 'moroa':
+      return 'burlywood';
+    case 'food':
+      return 'maroon';
+    case 'kava':
+      return 'indianred';
+    case 'pm':
+      return 'skyblue';
+    case 'op':
+      return 'royalblue';
+    default:
+      return 'black';
+  }
+};
 
 const Point = styled.div`
   position: absolute;
@@ -10,17 +34,31 @@ const Point = styled.div`
   left: ${({ x }) => (x || 0) - 12}px;
   width: 24px;
   height: 24px;
-  background-color: black;
+  background-color: ${getPointColorForType};
   border-radius: 50%;
+  color: white;
+  text-align: center;
+  line-height: 24px;
 `;
 
 const Map = styled.div`
   position: absolute;
   top: ${({ y }) => y || 0}px;
   left: ${({ x }) => x || 0}px;
-  background: url(${f13_img});
+  background: url(${level_img});
   width: 821px;
   height: 752px;
+`;
+
+const Player = styled.div`
+  position: absolute;
+  top: ${({ y }) => (y || 0) - 16}px;
+  left: ${({ x }) => (x || 0) - 16}px;
+  width: 32px;
+  height: 32px;
+  background-color: white;
+  border: solid 4px ${({ other }) => (other ? 'gray' : 'black')};
+  transition: all 0.5s;
 `;
 
 class App extends React.Component {
@@ -32,16 +70,97 @@ class App extends React.Component {
       others: {},
     };
   }
+  componentDidMount() {
+    this.username = prompt('Username?');
 
-  componentDidMount() {}
+    window.roll = this.roll;
+
+    this.socket = io('http://localhost:3000', {
+      transports: ['websocket', 'polling'],
+    });
+
+    this.socket.emit(`user:${this.user}`);
+
+    this.socket.on('msg', (msg) => {
+      console.log(msg);
+
+      if (msg.startsWith('host:')) {
+        if (msg.substring(5) === this.username) {
+          this.setState({ myTurn: true });
+        }
+      } else if (msg.startsWith('nextplayer:')) {
+        if (msg.substring(5) === this.username) {
+          this.setState({ myTurn: true });
+        }
+      } else if (msg.includes(':')) {
+        const { others } = this.state;
+        const [playername, posString] = msg.split(':');
+
+        const isNewUser = !Object.keys(others).includes(playername);
+
+        const oldPos = others[playername] || 0;
+        const position = parseInt(posString);
+
+        let diceValue = position - oldPos;
+        if (diceValue < 0) {
+          if (diceValue < -6) diceValue = -diceValue;
+          else diceValue += level.length;
+        }
+
+        if (isNewUser || diceValue > 6) {
+          this.setState({
+            others: { ...this.state.others, [playername]: position },
+          });
+        } else {
+          const goByOne = (i) => {
+            this.setState({
+              others: { ...this.state.others, [playername]: oldPos + i },
+            });
+            if (i < diceValue) setTimeout(() => goByOne(i + 1), 500);
+          };
+          goByOne(1);
+        }
+      }
+    });
+  }
+
+  roll = async (value) => {
+    const { pos } = this.state;
+    const npos = (pos + value) % level.length;
+    this.setState({ myTurn: false });
+    this.socket.emit('msg', `${this.username.replace(/:/g, '')}:${npos}`);
+
+    if (value > 6) {
+      this.setState({ pos: npos });
+    } else {
+      const goByOne = (i) => {
+        this.setState({ pos: (pos + i) % level.length });
+        if (i < value) setTimeout(() => goByOne(i + 1), 500);
+      };
+      goByOne(1);
+    }
+  };
 
   render() {
+    const { pos, others } = this.state;
     return (
       <div className="App">
         <Map>
-          {f13.map(([x, y, type]) => (
-            <Point x={x} y={y} />
+          {level.map(([x, y, type], i) => (
+            <Point x={x} y={y} type={type} key={`${x},${y}`}>
+              {i}
+            </Point>
           ))}
+          <Player x={level[pos][0]} y={level[pos][1]} />
+          {Object.entries(others)
+            .filter(([playername]) => playername !== this.username)
+            .map(([playername, position]) => (
+              <Player
+                key={playername}
+                x={level[position][0]}
+                y={level[position][1]}
+              />
+            ))}
         </Map>
       </div>
     );
